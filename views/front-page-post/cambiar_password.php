4c2a8ca26f15cf986160de0e08d41c4cedeff9a5
solucioneswebig <alexanderr677@gmail.com>
2020-07-02 13:28:49 -0400

    Modified

--- views/front-page-post/cambiar_password.php
+++ views/front-page-post/cambiar_password.php
@@ -17,7 +17,45 @@ $id = $user->ID;
 
     <div class="row">
         <div class="col-md-12">
-            <?php echo do_shortcode('[clean-login-edit]'); ?>
+        <div class="wpum-template wpum-form wpum-password-form">
+
+                <form action="/morgan/account/password" method="post" id="wpum-submit-password-form" enctype="multipart/form-data">
+
+                 <fieldset class="fieldset-password">
+
+                            
+                                <label for="password">
+                                    Contraseña													<span class="wpum-required">*</span>
+                                                        </label>
+                                <div class="field required-field">
+                                    
+                <input type="password" class="input-text" name="password" id="password" placeholder="" value="" maxlength="" required="">
+                                </div>
+
+                            
+                        </fieldset>
+                                <fieldset class="fieldset-password_repeat">
+
+                            
+                                <label for="password_repeat">
+                                    Repetir contraseña<span class="wpum-required">*</span>
+                                                        </label>
+                                <div class="field required-field">
+                                    
+                <input type="password" class="input-text" name="password_repeat" id="password_repeat" placeholder="" value="" maxlength="" required="">
+                                </div>
+
+                            
+                        </fieldset>
+                    
+                    <input type="hidden" name="wpum_form" value="password">
+                    <input type="hidden" name="step" value="0">
+                    <input type="hidden" id="password_change_nonce" name="password_change_nonce" value="0120d8245c"><input type="hidden" name="_wp_http_referer" value="/morgan/account/password">		<input type="submit" name="submit_password" class="button" value="Change password">
+
+                </form>
+
+
+                </div>
         </div>
     </div>    
 </div>
